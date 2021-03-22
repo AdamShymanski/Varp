20783118fd9223d093d9347f6c38e97e70bf20b2
Arnold <arnold.chan@lndata.com>
2021-03-22 12:59:45 +0800

    feat(client-landing): :sparkles: third element on landing page

---
+++ packages/client/src/react/elements/ThirdElementLanding.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import "./../../sass/elements/ThirdElementLanding-style.scss";
+import BatteryChargingFullIcon from '@material-ui/icons/BatteryChargingFull';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import GradeIcon from '@material-ui/icons/Grade';
+import FlareIcon from '@material-ui/icons/Flare';
+
+function ThirdElementLanding() {
+    return (
+        <article>
+        <div className='title poppinsFont flexRow'>
+            <div className='accentLine' />
+            <p>Third Element</p>
+          </div>
+          <div className='body poppinsFont'>
+            <p>
+              fill third element description here. fill third element description here .
+              fill third element description here .fill third element description here .
+              fill third element description here .fill third element description here
+            </p>
+          </div>
+        <div className='bottom flexRow'>
+            <div className='thirdCard flexColumn'>
+              <BatteryChargingFullIcon className='icon' />
+              <div className='cardTitleWrapper'>
+                <p className='cardTitle poppinsFont'>The power of our users</p>
+              </div>
+              <p className='description poppinsFont'>Our users can perform much more demanding tasks than users of, say, YouTube. This is due to the rewards they
+             can receive for completing these tasks. YouTube can only offer to watch another video, while Pyramid offers
+             players the chance to win a lot of money. This is a significant advantage of Pyramid over other services
+             offering advertising or, for example, filling out surveys.</p>
+            </div>
+            <div className='thirdCard flexColumn'>
+              <MonetizationOnIcon className='icon' />
+              <div className='cardTitleWrapper'>
+                <p className='cardTitle poppinsFont'>Other companies need us</p>
+              </div>
+              <p className='description poppinsFont'>Pyramid offers access to its own users who are difficult to reach through conventional methods. Our players
+             are young people who block Google ads. They are also not sensitive to Facebook ads. This is the reason why
+             Pyramid will be a great source of access to young people for other companies that care about this group.</p>
+            </div>
+            <div className='thirdCard flexColumn'>
+              <GradeIcon className='icon' />
+              <div className='cardTitleWrapper'>
+                <p className='cardTitle poppinsFont'>Pyramid has a much higher quality of its services</p>
+              </div>
+              <p className='description poppinsFont'>The quality of service, and the efficiency and effectiveness behind it, is much higher than the competition.
+            This is due to the fact that players are rewarded for performing tasks. A person who fills out a survey for
+            our collaborator, pays much more attention to the questions he asks because he knows that if he wins the
+            game, his answers will be checked by one of our administrators. And if it turns out that a user is filling
+            out the survey incorrectly, his prize will be rejected and will be given to another person.</p>
+            </div>
+            <div className='thirdCard flexColumn'>
+              <FlareIcon className='icon' />
+              <div className='cardTitleWrapper'>
+                <p className='cardTitle poppinsFont'>Pyramid is something new and interesting</p>
+              </div>
+              <p className='description poppinsFont'>Pyramid is something completely new to the market. It allows you to compete with other people for money,
+             without any input from you. This is much more exciting and interesting for young people than previous ways
+             of earning money for people who are under age and do not have any capital accumulated. In addition Pyramid
+             is very pleasant to play. One game can be joined by a group of friends, who will compete with each other for
+             the prize.</p>
+            </div>
+          </div>
+        </article>
+
+    )
+}
+
+export default ThirdElementLanding
