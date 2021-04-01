605ea3d3ae04aae307d6b164b95614d124bc0248
Dylan Simowitz <git@dylan.simowitz.com>
2021-04-01 14:39:21 -0700

    use webpack to load emotion pragma rather than babel

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,8 +1,31 @@
 module.exports = {
   reactOptions: {
     fastRefresh: true,
-    strictMode: true,
+    strictMode: true
   },
   stories: ["../lib/**/*.stories.mdx", "../lib/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  typescript: {
+    reactDocgen: "react-docgen"
+  },
+  webpackFinal: async config => {
+    config.module.rules[0].use[0].options.presets.splice(2, 1);
+    config.module.rules[0].use[0].options.presets = [
+      ...config.module.rules[0].use[0].options.presets,
+      [
+        require.resolve("@babel/preset-react"),
+        {
+          runtime: "automatic",
+          importSource: "@emotion/react"
+        }
+      ]
+    ];
+
+    config.module.rules[0].use[0].options.plugins = [
+      ...config.module.rules[0].use[0].options.plugins,
+      "@emotion/babel-plugin"
+    ];
+
+    return config;
+  }
 };
