f02a41b811f48dd40da71db856f94faba54f7605
Alvin Mantovani <alvin.ictn@gmail.com>
2021-05-07 11:55:11 +0700

    clear console

--- packages/client/src/react/SignInPage.tsx
+++ packages/client/src/react/SignInPage.tsx
@@ -32,7 +32,6 @@ export default function SignInPage() {
   });
 
   useEffect(() => {
-    console.log(history)
     console.log("TEST",currentUser !== null ? "YES" : "NO")
 
     currentUser !== null && history.push("/")
