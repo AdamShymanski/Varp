e7092027bff00cea3095fe7b84679820422b79ee
Adam Szymański <szymanskiadam111@gmail.com>
2021-05-10 14:38:41 +0200

    Function Build

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,76 +1,86 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = exports.createUserTest = exports.createUser = void 0;
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 // const serviceAccount = require('../keys/serviceAccountKey.json');
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createUserTest = exports.fetchUserData = void 0;
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
-exports.createUser = functions.https.onCall((data) => {
-    try {
-        const { email, name, password, age, contry } = data;
-        admin
-            .auth()
-            .createUser({
-            email: email,
-            emailVerified: false,
-            password: password,
-            displayName: name,
-            disabled: false,
-        })
-            .then((currentUser) => {
-            console.log('Successfully created new user');
-            // Creating user's doc with custom info
-            admin
-                .firestore()
-                .collection('users')
-                .doc(currentUser.uid)
-                .set({
-                age: age,
-                contry: contry,
-                dailyStreak: 0,
-                surveyProgram: 0,
-                referralProgram: 0,
-                referralCode: currentUser.uid,
-            })
-                .then(() => {
-                console.log('Document successfully written!');
-                return currentUser;
-            })
-                .catch((error) => {
-                console.error('Error writing document: ', error);
-                return error;
-            });
-        })
-            .catch((error) => {
-            console.log('Error creating new user:', error);
-            return error;
-        });
-    }
-    catch (error) {
-        return error;
-    }
-});
+// export const createUser = functions.https.onCall((data: RegisterProps) => {
+//   try {
+//     const {email, name, password, age, contry} = data;
+//     admin
+//       .auth()
+//       .createUser({
+//         email: email,
+//         emailVerified: false,
+//         password: password,
+//         displayName: name,
+//         disabled: false,
+//       })
+//       .then((currentUser: {uid: string}) => {
+//         console.log('Successfully created new user');
+//         // Creating user's doc with custom info
+//         admin
+//           .firestore()
+//           .collection('users')
+//           .doc(currentUser.uid)
+//           .set({
+//             balance: 0,
+//             age: age,
+//             contry: contry,
+//             dailyStreak: 0,
+//             surveyProgram: 0,
+//             referralProgram: 0,
+//             referralCode: currentUser.uid,
+//           })
+//           .then(() => {
+//             console.log('Document successfully written!');
+//             return currentUser;
+//           })
+//           .catch((error) => {
+//             console.error('Error writing document: ', error);
+//             return error;
+//           });
+//       })
+//       .catch((error) => {
+//         console.log('Error creating new user:', error);
+//         return error;
+//       });
+//   } catch (error) {
+//     return error;
+//   }
+// });
 // : {email: string; password: string}
+exports.fetchUserData = functions.https.onCall(async (data) => {
+    const result = await db.collection('users').doc(data.uid).get();
+    return result.data();
+});
 exports.createUserTest = functions.https.onCall(async (data) => {
-    const { email, password } = data;
-    console.log(email);
-    console.log(password);
+    const { email, password, age, name, country } = data;
     try {
         const user = await admin.auth().createUser({
             email: email,
             emailVerified: false,
             password: password,
+            displayName: name,
             disabled: false,
         });
-        console.log(user);
-        const userDoc = db.collection('users').doc(user.uid);
-        await userDoc.set({
-            name: 'Alan',
+        await db.collection('users').doc(user.uid).set({
+            balance: 0,
+            age: age,
+            contry: country,
+            dailyStreak: 0,
+            surveyProgram: 0,
+            referralProgram: 0,
+            referralCode: user.uid,
+            lastAction: '',
+            name: name,
         });
+        return { result: 'success' };
     }
     catch (error) {
         if (error.code === 'auth/email-already-exists') {
@@ -83,9 +93,5 @@ exports.createUserTest = functions.https.onCall(async (data) => {
             // the message INTERNAL and the code internal.
         }
     }
-});
-exports.test = functions.https.onCall((data) => {
-    console.log('test log test log test log test log');
-    return { result: `success ${data}` };
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
 // const serviceAccount = require('../keys/serviceAccountKey.json');
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // });
+
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
@@ -13,94 +13,106 @@ const db = admin.firestore();
 
 interface RegisterProps {
   email: string;
-  name: string;
   password: string;
+  name: string;
   age: number;
-  contry: string;
+  country: string;
+}
+interface FUDProps {
+  uid: string;
 }
 
-export const createUser = functions.https.onCall((data: RegisterProps) => {
-  try {
-    const {email, name, password, age, contry} = data;
-    admin
-      .auth()
-      .createUser({
+// export const createUser = functions.https.onCall((data: RegisterProps) => {
+//   try {
+//     const {email, name, password, age, contry} = data;
+//     admin
+//       .auth()
+//       .createUser({
+//         email: email,
+//         emailVerified: false,
+//         password: password,
+//         displayName: name,
+//         disabled: false,
+//       })
+//       .then((currentUser: {uid: string}) => {
+//         console.log('Successfully created new user');
+//         // Creating user's doc with custom info
+//         admin
+//           .firestore()
+//           .collection('users')
+//           .doc(currentUser.uid)
+//           .set({
+//             balance: 0,
+//             age: age,
+//             contry: contry,
+//             dailyStreak: 0,
+//             surveyProgram: 0,
+//             referralProgram: 0,
+//             referralCode: currentUser.uid,
+//           })
+//           .then(() => {
+//             console.log('Document successfully written!');
+//             return currentUser;
+//           })
+//           .catch((error) => {
+//             console.error('Error writing document: ', error);
+//             return error;
+//           });
+//       })
+//       .catch((error) => {
+//         console.log('Error creating new user:', error);
+//         return error;
+//       });
+//   } catch (error) {
+//     return error;
+//   }
+// });
+
+// : {email: string; password: string}
+
+export const fetchUserData = functions.https.onCall(async (data: FUDProps) => {
+  const result = await db.collection('users').doc(data.uid).get();
+  return result.data();
+});
+
+export const createUserTest = functions.https.onCall(
+  async (data: RegisterProps) => {
+    const {email, password, age, name, country} = data;
+
+    try {
+      const user = await admin.auth().createUser({
         email: email,
         emailVerified: false,
         password: password,
         displayName: name,
         disabled: false,
-      })
-      .then((currentUser: {uid: string}) => {
-        console.log('Successfully created new user');
-        // Creating user's doc with custom info
-        admin
-          .firestore()
-          .collection('users')
-          .doc(currentUser.uid)
-          .set({
-            balance: 0,
-            age: age,
-            contry: contry,
-            dailyStreak: 0,
-            surveyProgram: 0,
-            referralProgram: 0,
-            referralCode: currentUser.uid,
-          })
-          .then(() => {
-            console.log('Document successfully written!');
-            return currentUser;
-          })
-          .catch((error) => {
-            console.error('Error writing document: ', error);
-            return error;
-          });
-      })
-      .catch((error) => {
-        console.log('Error creating new user:', error);
-        return error;
       });
-  } catch (error) {
-    return error;
-  }
-});
 
-// : {email: string; password: string}
-export const createUserTest = functions.https.onCall(async (data) => {
-  const {email, password, age, name, country} = data;
+      await db.collection('users').doc(user.uid).set({
+        balance: 0,
+        age: age,
+        contry: country,
+        dailyStreak: 0,
+        surveyProgram: 0,
+        referralProgram: 0,
+        referralCode: user.uid,
+        lastAction: '',
+        name: name,
+      });
 
-  console.log(email);
-  console.log(password);
-  try {
-    const user = await admin.auth().createUser({
-      email: email,
-      emailVerified: false,
-      password: password,
-      displayName: name,
-      disabled: false,
-    });
-
-    db.collection('users').doc(user.uid).set({
-      balance: 0,
-      age: age,
-      contry: country,
-      dailyStreak: 0,
-      surveyProgram: 0,
-      referralProgram: 0,
-      referralCode: user.uid,
-    });
-    return {result: 'success'};
-  } catch (error) {
-    if (error.code === 'auth/email-already-exists') {
-      throw new functions.https.HttpsError(
-        'already-exists',
-        'The provided email is already in use by an existing user',
-      );
-    } else {
-      // throw new functions.https.HttpsError("...other code....", "...");
-      // If an error other than HttpsError is thrown,
-      // your client instead receives an error with
-      // the message INTERNAL and the code internal.
+      return {result: 'success'};
+    } catch (error) {
+      if (error.code === 'auth/email-already-exists') {
+        throw new functions.https.HttpsError(
+          'already-exists',
+          'The provided email is already in use by an existing user',
+        );
+      } else {
+        // throw new functions.https.HttpsError("...other code....", "...");
+        // If an error other than HttpsError is thrown,
+        // your client instead receives an error with
+        // the message INTERNAL and the code internal.
+      }
     }
-  }
-});
+  },
+);
