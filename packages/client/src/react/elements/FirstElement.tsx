8347553430cb2afd93f9e22322832d7992e368c5
Adam Szymański <szymanskiadam111@gmail.com>
2021-05-13 20:20:49 +0200

    text: 📑info about Varp

--- packages/client/src/react/elements/FirstElement.tsx
+++ packages/client/src/react/elements/FirstElement.tsx
@@ -25,40 +25,90 @@ function FirstElement(props: any) {
         </div>
         <div className="body poppinsFont">
           <p>
-            This will be a web application where young people can compete in
-            arcade games like Pac-Man, Icy Tower, Snake and so on for money.
+            This will begin to be a better way to earn casual income than the
+            current sites that allow you to earn on CPA (Cost Per Action)
+            marketing.
           </p>
           <p>
-            Overall simple, easy games. Varp will be an{' '}
-            <span>alternative to tediously filling out surveys</span> on
-            Swagbucks type sites.
+            Current offers are around $1.5 for a survey that takes 20-30 minutes
+            to complete and the rewards are not paid in cash anyway, but e.g. in
+            the form of vouchers to stores. This is terrible! Varp greatly
+            reduces the time it takes to complete a task to receive a reward,
+            and all money is paid out as a transfer to the user's bank account.
+            Our model is based on adding an element of competition for the
+            prize. The potential earnings of all users are added together and
+            form a pool to be divided among several winners. Winners are
+            determined through a game consisting of several smaller stages in
+            which players play arcade games (Snake, Flappy Bird, Galaga, etc.).
+            We chose this method of competition because it is relatively fair,
+            interesting, engaging, and fun.
+          </p>
+
+          <ul>
+            <li>
+              <p>The example scenario of the game-</p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>Number of players: 400
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>Number of winners: 6
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>Pool: 300$
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>Prize received by each winner: 50$
+              </p>
+            </li>
+          </ul>
+
+          <p>
+            With each stage, half of the players with the lowest score are
+            dropped. At the end there are only a few winners who receive their
+            share of the pot. It is worth reminding that to join the game it is
+            enough to spend about 15 minutes.
           </p>
           <p>
-            Varp adds <span>risk</span> and an element of competition between
-            users to this model. We noticed that young people want to compete
-            for money rather than have a certain profit.
+            <span>Pretty good deal, right?</span>
           </p>
-          <p>
-            In order to take part in the game, the player has to perform several
-            tasks consisting of visiting the sponsor's website, filling in a
-            short questionnaire and other such tasks. Time to complete the tasks
-            will not exceed <span>10 minutes</span>.
-          </p>
-          <p>
-            After completing them you will receive tokens for which you will be
-            able to take part in the game In Varp the potential earnings from
-            the tasks of all the players are added together{' '}
-            <span>creating a pool </span>
-            which only a few winners will win.
-          </p>
-          <p>
-            This way, players can get their money much <span>faster</span> and
-            in a <span>more interesting way</span>. All prizes are paid out in{' '}
-            <span>cash</span>.
-          </p>
+
+          <ul>
+            <li>
+              <p>What do you get when you use Varp:</p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>You save your time
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>Competition adds great excitement
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>You do not have to perform the same tedious tasks
+                over and over again
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>- </span>You receive real cash
+              </p>
+            </li>
+          </ul>
         </div>
       </article>
-      <article>
+      {/* <article>
         <div className="sTitle title poppinsFont flexRow">
           <div className="accentLine" />
           <p>What Varp've already achieved?</p>
@@ -101,7 +151,7 @@ function FirstElement(props: any) {
             </div>
           </div>
         </div>
-      </article>
+      </article> */}
     </main>
   );
 }
