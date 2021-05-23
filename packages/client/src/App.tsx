43242bf816ee59db4ef57d1c1cf1412614a9d97d
Adam Szymański <szymanskiadam111@gmail.com>
2021-05-23 21:12:40 +0200

    route: 🏎 Settings Page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,17 +12,20 @@ import ContactPage from './react/ContactPage';
 import LandingPage from './react/LandingPage';
 import SignInPage from './react/SignInPage';
 import MainPage from './react/MainPage';
+import SettingsPage from './react/SettingsPage';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <div className="appComponent">
-          <PrivateRoute path="/" exact component={MainPage} />
+          <SettingsPage />
+          {/* <PrivateRoute path="/" exact component={MainPage} />
           <PrivateRoute path="/home" component={LandingPage} />
           <PrivateRoute path="/register" component={RegisterPage} />
           <PrivateRoute path="/sign-in" component={SignInPage} />
           <PrivateRoute path="/contact" component={ContactPage} />
+          <PrivateRoute path="/settings" component={SettingsPage} /> */}
         </div>
       </AuthProvider>
     </Router>
