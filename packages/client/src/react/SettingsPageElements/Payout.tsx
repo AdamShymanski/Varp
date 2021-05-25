80ca9a45f0f6678dea4335d2f36f62ae090f5f3a
Alvin Mantovani <alvin.ictn@gmail.com>
2021-05-25 23:07:36 +0700

    Make some change

--- packages/client/src/react/SettingsPageElements/Payout.tsx
+++ packages/client/src/react/SettingsPageElements/Payout.tsx
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 
-const Payout:React.FC = (params:any) => {
+interface Props {
+  params: any
+}
+const Payout:React.FC<Props> = (params:any) => {
   return (
     <main className={`${params.elementState == 2 ? 'show' : 'hide'}`}>
       <p className="poppinsFont">Payout</p>
