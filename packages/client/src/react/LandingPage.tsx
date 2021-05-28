8106ad550fa0a03b5b34d260b4ec1863b62ef60a
Adam Szymański <szymanskiadam111@gmail.com>
2021-05-28 20:12:35 +0200

    landing-page: 🎉 complete rebuild

--- packages/client/src/react/LandingPage.tsx
+++ packages/client/src/react/LandingPage.tsx
@@ -7,12 +7,15 @@ import {Button} from '@varp/ui';
 
 //resources
 import logo from './../resources/icons/logo.png';
+import mockup from './../resources/mockup.png';
+
 import hamburger from './../resources/icons/hamburger-menu.svg';
 
-//elements
-import FirstElement from './elements/FirstElement';
-import SecondElement from './elements/SecondElement';
-import ThirdElement from './elements/ThirdElement';
+import clock from './../resources/illustrations/clock.png';
+import hand from './../resources/illustrations/hand_money.png';
+import podium from './../resources/illustrations/podium.png';
+import file from './../resources/illustrations/lock_file.png';
+import inputs from './../resources/illustrations/inputs_ill.png';
 
 function LandingPage() {
   const history = useHistory();
@@ -21,76 +24,112 @@ function LandingPage() {
   const pushToSignIn = () => history.push('/sign-in');
   const pushToContact = () => history.push('/contact');
 
-  const [visibilityState, setVisibilityState] = useState(1);
-
-  const buttonVisibilityHandler = (hideNumber: number) => {
-    if (hideNumber === visibilityState) {
-      return false;
-    }
-    return true;
-  };
-
-  const [hamburgerState, setHamburger] = useState(false);
-
   return (
-    <div className={`wrapper ${hamburgerState && 'fullscreen'}`}>
-      <div className="wrapper--navbar flexRow">
-        <img
-          src={logo}
-          alt="Logo"
-          className="logo"
-          onClick={() => {
-            history.push('/home');
-          }}
-        />
-        <ul className="flexRow">
-          <div
-            className={`default-menu flexRow ${
-              hamburgerState && 'expand-menu'
-            }`}
-          >
-            <li className="poppinsFont">For Developers</li>
-            <li className="poppinsFont" onClick={pushToContact}>
-              {' '}
-              Contact
-            </li>
-            <li className="poppinsFont" onClick={pushToRegister}>
-              Register
-            </li>
-            <li className="poppinsFont" onClick={pushToSignIn}>
-              Sign In
-            </li>
-          </div>
-          <div className="hamburgerMenu">
-            <img
-              src={hamburger}
-              alt="Hamburger Menu"
-              onClick={() => setHamburger(!hamburgerState)}
-            />
-          </div>
-          <li className="poppinsFont " onClick={pushToRegister}>
-            <Button variant="primary" size="medium" children="Register" />
-          </li>
-          <li className="poppinsFont ">
+    <div className="lpWrapper flexColumn">
+      <header className="flexRow">
+        <img src={logo} />
+        <aside className="flexRow">
+          <ul className="headerList flexRow poppinsFont">
+            <li>Collaboration</li>
+            <li>Business</li>
+            <li>Contact</li>
+          </ul>
+          <div className="flexRow btnWrapper">
+            <Button />
             <p>or</p>
-          </li>
-          <li className="poppinsFont " onClick={pushToSignIn}>
-            <Button variant="primary" size="medium" children="Sign In" />
-          </li>
-        </ul>
-        <article
-          className={`sideDrawer ${hamburgerState ? 'invisible' : ''}`}
-        ></article>
-      </div>
-      <div className="wrapper--main flexColumn">
-        <FirstElement />
-        <SecondElement />
-        <ThirdElement />
-      </div>
-      <footer className="flexColumn robotoFont">
-        <p>© 2021 Varp.io</p>
-        <p>All rights reserved.</p>
-      </footer>
+            <Button />
+          </div>
+        </aside>
+      </header>
+      <main className="flexColumn poppinsFont mainSection">
+        <h1>Earn money for performing simple tasks</h1>
+        <p>
+          We're turning the CPA marketing monetization model upside down. We add
+          an element of competition, shorten the time and significantly improve
+          the overall comfort of this way of earning. Even $20 in 15 minutes.
+        </p>
+        <Button size="huge" />
+        <div className="mockupWrapper flexRow">
+          <div className="shadowHandler1" />
+          <img src={mockup} />
+          <div className="shadowHandler2" />
+        </div>
+      </main>
+      <article className="featuresArt poppinsFont">
+        <div className="flexRow">
+          <img src={hand} className="hand" />
+          <div className="featureContent">
+            <h3>Payouts in real cash</h3>
+            <p>
+              <span>More freedom.</span> If you win we just pay you your money,
+              we don't care where or what you spend it on, it's your choice.
+            </p>
+          </div>
+        </div>
+        <div className="flexRow">
+          <img src={clock} className="clock" />
+          <div className="featureContent">
+            <h3>Up to 3 times faster</h3>
+            <p>
+              <span>Competition is our way.</span> By taking up a fight for a
+              prize in arcade games, we can reduce the time you would have to
+              spend to get a prize.
+            </p>
+          </div>
+        </div>
+        <div className="flexRow">
+          <img src={file} className="file" />
+          <div className="featureContent">
+            <h3>Your data is safe</h3>
+            <p>
+              <span>Enhanced privacy.</span> Although you share your data with
+              other companies you can fully control the content that is sent to
+              them. Even data already stored by us must be authorized by you
+              before it is shared.
+            </p>
+          </div>
+        </div>
+        <div className="flexRow">
+          <img src={podium} className="podium" />
+          <div className="featureContent">
+            <h3>Competitive element</h3>
+            <p>
+              <span>Add some excitement.</span> With arcade games competition
+              you also get to enjoy playing against other players, it really is
+              addictive.
+            </p>
+          </div>
+        </div>
+      </article>
+      <article className="nrpArt flexRow">
+        <div className="textContainer poppinsFont">
+          <h4>TIME SAVER</h4>
+          <h1>We remember your answers</h1>
+          <p>
+            <span>Increased efficiency.</span> By saving previous responses, we
+            can use
+            <span className="accentSpan"> AutoFill</span> to reduce the time
+            needed to complete the survey by up to 40%.
+          </p>
+        </div>
+        <img src={inputs} />
+        <aside className="inputIllustration"></aside>
+      </article>
+      <article className="faqArt flexRow">
+        <div className="textContainer poppinsFont">
+          <h4>GET TO KNOW VARP</h4>
+          <h1>Frequently Asked Questions</h1>
+          <p>
+            <span>Increased efficiency.</span> By saving previous responses, we
+            can use
+            <span className="accentSpan"> AutoFill</span> to reduce the time
+            needed to complete the survey by up to 40%.
+          </p>
+        </div>
+        <aside></aside>
+      </article>
+      <article className="complicatedArt"></article>
+      <footer></footer>
     </div>
   );
 }
